7296bf8c9f50279d651548876f255b7e08ef7ee0
chalyong <kok3335@naver.com>
2026-09-23 19:00:12 +0900

    캐릭터 위치 x, y 변수로 교체

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -7,7 +7,9 @@ def move_circle():
     while rad < 360:
         clear_canvas()
         grass.draw(400, 30)
-        character.draw(400, 300)
+        x = 400
+        y = 300
+        character.draw(x, y)
         update_canvas()
         rad += 0.1
     pass
