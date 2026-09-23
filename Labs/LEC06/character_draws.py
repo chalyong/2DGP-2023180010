f50c651671eaed5c1d65a4698909ff52e449905f
chalyong <kok3335@naver.com>
2026-09-23 19:10:48 +0900

    math 함수 추가 및 반복문 라디안 기준으로 조건 변경.

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,10 +1,11 @@
 # 실습 과제 진행
+import math
 from pico2d import *
 
 def move_circle():
     print("circle")
     rad = 0
-    while rad < 360:
+    while rad < 2 * math.pi:
         clear_canvas()
         grass.draw(400, 30)
         x = 400 + 200 * math.cos(rad)
