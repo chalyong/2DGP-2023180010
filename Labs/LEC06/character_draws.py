882a544a60c89624e2160e31667d80af09bf3643
chalyong <kok3335@naver.com>
2026-09-23 18:24:46 +0900

    pico2d import & 화면 띄우기

--- Labs/LEC06/character_draws.py
+++ Labs/LEC06/character_draws.py
@@ -1,4 +1,5 @@
 # 실습 과제 진행
+from pico2d import *
 
 def move_circle():
     print("circle")
@@ -12,8 +13,12 @@ def move_triangle():
     print("triangle")
     pass
 
+open_canvas(800, 600)
+
 while True:
     move_circle()
     move_rectangle()
     move_triangle()
     pass
+
+close_canvas()
